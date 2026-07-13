ca595a121ce49d39e8f256f3b0038aa4dd159caa
jf796 <157021612+jf796@users.noreply.github.com>
2026-07-13 15:51:47 +0100

    Setting original to work on GPU

--- xray_separation_original.py
+++ xray_separation_original.py
@@ -1,5 +1,13 @@
 import numpy as np
 import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 from PIL import Image
 
 
